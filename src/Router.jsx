0f869770ad7cdbaf053d4ff82eaf81b4564ae6e9
Nolwenn <bohelay.nolwenn@gmail.com>
2024-12-20 10:02:35 +0100

    build (footer): ajout du footer

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,20 +2,21 @@ import { createBrowserRouter } from "react-router-dom"
 import Home from './Home/index.jsx'
 import About from "./About/about.jsx"
 import Header from "./Header/header.jsx"
+import Footer from "./Footer/Footer.jsx"
 import Error from "./Error/error.jsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (<><Header /> <Home /></>),
+    element: (<><Header /> <Home /> <Footer /> </>),
   },
   {
     path: "/about",
-    element: (<><Header /> <About /></>),
+    element: (<><Header /> <About /> <Footer /> </>),
   },
   {
     path: "*",
-    element: <Error />,
+    element: (<><Header /> <Error /> <Footer /> </>),
   },
 ]);
 
