8eac07ef8d77bbc8d471e61e475f0bc6094172f8
Nolwenn <bohelay.nolwenn@gmail.com>
2024-12-26 12:42:00 +0100

    build (banner): composant banner + style

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,15 +4,16 @@ import About from "./About/about.jsx"
 import Header from "./Header/header.jsx"
 import Footer from "./Footer/Footer.jsx"
 import Error from "./Error/error.jsx"
+import Banner from "./Banner/Banner.jsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (<><Header /> <Home /> <Footer /> </>),
+    element: (<><Header /> <Home /> <Banner page="Home" /> <Footer /> </>),
   },
   {
     path: "/about",
-    element: (<><Header /> <About /> <Footer /> </>),
+    element: (<><Header /> <About /> <Banner page="About" /> <Footer /> </>),
   },
   {
     path: "*",
--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,7 @@
 function About (){
     return(
         <div>
-           <h1>À propos</h1>
+           <h1></h1>
         </div>
     )
 }
